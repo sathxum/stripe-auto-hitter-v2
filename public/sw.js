b84beb8a1c9967b6200e7c52903e04c5625e691f
sathxum <sinketpr.o@proton.me>
2026-03-31 23:34:21 +0530

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+// Service Worker for Stripe Auto-Hitter PWA
+const CACHE_NAME = 'stripe-hitter-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/app.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
